5cd509f7aa2e614f136f5e5a79a166cbcf54c72b
macchu <matt.rasmusson@gmail.com>
2022-06-06 14:59:35 -0500

    The other_school scope returns enrollments for schools other than the one provided.

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -7,4 +7,5 @@ class Enrollment < ApplicationRecord
   validates :school, :cohort, presence: true
 
   scope :active, -> { where('start_date <= ? AND end_date >= ?', Time.now, Time.now) }
+  scope :other_school, -> (this_school_id) { where.not(school_id: this_school_id)}
 end
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe Enrollment do
-  let(:school) { create(:school) }
+  let(:school) { create(:school, name: "Public 1") }
   let(:yesterday) { Time.now.yesterday }
   let(:last_year) { Time.now.last_year }
   let(:tomorrow) { Time.now.tomorrow }
   
-  describe ".active" do
+  describe "#active" do
     context "One inactive enrollment" do
       let!(:enrollment_1) { create(:enrollment, school: school, start_date: last_year, end_date: yesterday) }
 
@@ -24,4 +24,17 @@ describe Enrollment do
       end
   end
 
+  describe "#other_school" do
+    let!(:enrollment_1) { create(:enrollment, school: school, start_date: last_year, end_date: tomorrow) }
+    let(:school_2) { create(:school, name: "Private 1") }
+    let!(:enrollment_2) { create(:enrollment, school: school_2, start_date: last_year, end_date: tomorrow)}
+    
+    it "returns enrollment_1 when given school_2" do
+      expect(Enrollment.other_school(school_2.id)).to include enrollment_1
+    end
+
+    it "returns enrollment_2 when given school_1" do
+      expect(Enrollment.other_school(school.id)).to include enrollment_2
+    end
+  end
 end
