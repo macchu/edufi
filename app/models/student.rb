d2e52e1ad00127f7bfc6521644a8055d40e1ff8a
macchu <matt.rasmusson@gmail.com>
2022-06-06 15:34:16 -0500

    Do not allow school_id to change if the student has active_enrollments in the current school.

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,7 @@ class Student < ApplicationRecord
   has_many :cohorts, through: :enrollments
 
   validates :school, presence: true
+  validate :no_active_enrollments_before_transfer
 
   def full_name
     "#{first_name} #{last_name}"
@@ -14,6 +15,14 @@ class Student < ApplicationRecord
   end
 
   def active_enrollment?
-    !self.enrollments.active.empty?
+    self.enrollments.active.empty?
+  end
+
+  private 
+  def no_active_enrollments_before_transfer
+    actives  = self.enrollments.active.other_school(self.school_id)
+    if !actives.empty?
+      errors.add(:school_id, "Student has an active enrollment in a different school.")
+    end
   end
 end
