d4a5de6336713c1032c4702291b4e835c612532d
macchu <matt.rasmusson@gmail.com>
2022-06-07 10:33:18 -0500

    Return all schools except a specific school. Useful for excluding a student's current school from transfer school options.

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,4 +3,6 @@ class School < ApplicationRecord
   has_many :students, dependent: :destroy
   has_many :cohorts, dependent: :destroy
   has_many :teachers, dependent: :destroy
+
+  scope :except_school, -> (school_id) { where.not(id: school_id)}
 end
