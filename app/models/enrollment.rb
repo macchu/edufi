4712ea19385cc6f3e91ea81eb0bbacb5f3804d05
macchu <matt.rasmusson@gmail.com>
2022-06-07 14:02:41 -0500

    N-bullet recommended eager loading here for the student/:id

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -6,6 +6,6 @@ class Enrollment < ApplicationRecord
   validates :student, presence: true, uniqueness: { scope: :cohort_id }
   validates :school, :cohort, presence: true
 
-  scope :active, -> { where('start_date <= ? AND end_date >= ?', Time.now, Time.now) }
+  scope :active, -> { where('start_date <= ? AND end_date >= ?', Time.now, Time.now).includes(:cohort) }
   scope :other_school, -> (this_school_id) { where.not(school_id: this_school_id)}
 end
