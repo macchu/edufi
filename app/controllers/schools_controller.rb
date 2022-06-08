ae29752a12d83fc69848eb531638730cd222a09b
macchu <matt.rasmusson@gmail.com>
2022-06-08 12:54:09 -0500

    Eager load students and cohorts trhough enrollments

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -59,7 +59,7 @@ class SchoolsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_school
-      @school = School.find(params[:id])
+      @school = School.includes(enrollments: [:student, :cohort]).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
